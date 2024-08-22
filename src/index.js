192df16707958c16dc5bd143b2ef15548699f0a2
Anand Raj <cse.20bcse33@silicon.ac.in>
2024-08-23 01:02:41 +0530

    added utilities

--- src/index.js
+++ src/index.js
@@ -1,16 +1,23 @@
 // require('dotenv').config({path:'./env'})
 
 import dotenv from 'dotenv'
-import express from 'express'
+
+
 import connectDB from "./db/index.js"
+import app from './app.js'
 
 dotenv.config({
     path:'./env'
 })
 
 connectDB()
-const app = express()
+.then(()=>{
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`Server is running on port ${process.env.PORT}`)
+    })
 
-app.listen(process.env.PORT,()=>{
-    console.log(`Server is running on port ${process.env.PORT}`)
+})
+.catch((err)=>{
+    console.log("Error in connection ", err);
+    
 })
